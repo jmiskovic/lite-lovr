50428265ea817881dea1222263afea6be36bef93
Josip Miskovic <josipmiskovic@gmail.com>
2021-12-05 10:30:32 +0100

    Resize desktop window

--- src/conf.lua
+++ src/conf.lua
@@ -1,7 +1,9 @@
 lovr.conf = function(t)
   t.title, t.identity = "lite", "lite"
   t.saveprecedence = true
-  t.window.width = 1000
-  t.window.height = 1000
-  t.modules.headset = not true -- with `false` on desktop the camera is fixed 
+  t.window.width = 1920
+  t.window.height = 1080
+  t.window.fullscreen = true
+  t.modules.headset = true -- set to `false` to fix the camera on desktop simulator
+  --t.window.vsync = 0
 end
